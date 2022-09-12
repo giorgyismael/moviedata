63274221b0b3fffbc8a430efc2c3b36d4b98da74
Giorgy Oliveira <giorgy.oliveira@neogrid.com>
2022-09-12 10:43:16 -0300

    ajuste do message response

--- src/main/java/com/movies/data/moviesdata/exception/FileUploadExceptionAdvice.java
+++ src/main/java/com/movies/data/moviesdata/exception/FileUploadExceptionAdvice.java
@@ -13,6 +13,6 @@ public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
-    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
+    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!", HttpStatus.EXPECTATION_FAILED.value()));
   }
 }
